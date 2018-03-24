7af9476e806dad02401f7463d40c25e2c148f711
ridouchire <ridouchire@gmail.com>
2018-03-24 15:54:43 +0400

    solving problem 17.

---
+++ problem17.lisp
@@ -0,0 +1,11 @@
+(defun problem17 (n)
+  (let ((cnt 0))
+    (loop
+     :for i from 1 upto n
+     :do (incf cnt
+	       (+ (count-if #'(lambda (c)
+				(and (char/= #\Space c)
+				     (char/= #\- c)))
+			    (format nil "+~R" i))
+		  (if (and (> i 100) (/= 0 (rem i 100))) 3 0))))
+    cnt))
